87597f47eec096e1a5eecaf6ff40823d66e26874
yejinleee <yejin9487@gmail.com>
2023-10-31 13:34:21 +0900

    1031 기본 컴포넌트 구조

---
+++ VanillaJS/31_catAlbum/src/Nodes.js
@@ -0,0 +1,42 @@
+export default function Nodes({ $target, initialState, onClick }) {
+  const $nodes = document.createElement("div");
+  $nodes.classList.add("nodes");
+  $target.appendChild($nodes);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    const { isRoot, nodes } = this.state;
+    $nodes.innerHTML = `
+      ${
+        isRoot
+          ? ""
+          : `
+      <div class="Node">
+        <img src="http://cdn.roto.codes/images/prev.png">
+      </div>
+      `
+      }
+      ${nodes
+        .map(
+          (node) => `
+          <div class="Node">
+            <img src="${
+              node.type === "DIRECTORY"
+                ? "https://cdn.roto.codes/images/directory.png"
+                : "https://cdn.roto.codes/images/file.png"
+            }">
+            ${node.name}
+          </div>
+        `
+        )
+        .join("")}
+    `;
+  };
+  this.render();
+}
---
+++ VanillaJS/31_catAlbum/src/main.js
@@ -0,0 +1,5 @@
+import App from "./App.js";
+
+const $target = document.getElementById("App");
+
+new App({ $target });
