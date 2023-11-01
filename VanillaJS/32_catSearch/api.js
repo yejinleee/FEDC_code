fe7d2170a7b9c6572ecbe5c147b7dda1126a644f
yejinleee <yejin9487@gmail.com>
2023-11-01 14:17:36 +0900

    1101 기본 컴포넌트 구조

---
+++ VanillaJS/32_catSearch/api.js
@@ -0,0 +1,4 @@
+export const KEYWORD_API_END_POINT =
+  "https://kdt-frontend.cat-search-api.programmers.co.kr/api/cats/keywords?q=";
+export const PHOTO_API_END_POINT =
+  "https://kdt-frontend.cat-search-api.programmers.co.kr/api/cats/search?q=";
