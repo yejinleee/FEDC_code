253a1aa339b4fa148264e4ee4a6173517faca726
yejinleee <yejin9487@gmail.com>
2023-11-23 18:16:19 +0900

    1123 parcel

---
+++ Vue/48_Vue/vue-start-parcel/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "hello~",
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+$color: orange;
+h1 {
+  color: $color;
+}
+</style>
---
+++ Vue/48_Vue/vue-start-parcel/src/main.js
@@ -0,0 +1,4 @@
+import * as Vue from "vue"; // 경로아닌 이렇게 from 이면 node_modules에서 가져오는 것!
+import App from "./App.vue";
+
+Vue.createApp(App).mount("#app");
