d161520a874b71ed0d7749610339f64951bf0f2d
yejinleee <yejin9487@gmail.com>
2023-11-23 22:04:26 +0900

    1123vue wepback  dev

--- Vue/48_Vue/vue-start-webpack/src/App.vue
+++ Vue/48_Vue/vue-start-webpack/src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <h1>{{ msg }}</h1>
+  <Hello />
 </template>
 
 <script>
+import Hello from "~/components/Hello";
+
 export default {
+  components: {
+    Hello,
+  },
   data() {
     return {
       msg: "Hello webpack",
--- Vue/48_Vue/vue-start-webpack/webpack.config.js
+++ Vue/48_Vue/vue-start-webpack/webpack.config.js
@@ -1,8 +1,15 @@
 const path = require("path"); // 기본내장기능 path 가져옴
 const { VueLoaderPlugin } = require("vue-loader"); // vue-loader설치후 사용가능. 플러그인 에러해결하기위함
 const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  resolve: {
+    extensions: [".vue", ".js"], // 확장자
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    }, // 경로 별칭
+  },
   entry: "./src/main.js", // 웹팩이 해석할때 진입점
   output: {
     // path: 결과물 반환 위치, filename :파일의 이름(기본은 위의 entry js명)
@@ -18,6 +25,14 @@ module.exports = {
         test: /\.vue$/, // 정규표현식을 통해 .vue 찾음 // 정규표현식.text()로 탐지하는 역할
         use: "vue-loader", // js만 해석할 수 있는 webpack이 vue도 해석할 수 있도록 하는 패키지/ npm i -D vue-loader@next
       },
+      {
+        test: /\.s?css$/,
+        use: [
+          "vue-style-loader",
+          "css-loader", // 순서 중요! 아래부터, 오른쪽부터 평가됨. 먼저 해석되어야 하는 로더를 마지막에 작성!!!!
+          "sass-loader",
+        ],
+      },
     ],
   },
   plugins: [
@@ -25,5 +40,16 @@ module.exports = {
     new HtmlPlugin({
       template: "./src/index.html", //해석할 파일 경로. 이또한 path.resolve사용한 절대경로로 지정이 필요한대 내부적으로 실행해쥼. path.resolve(__dirname, "./src/index.html") 와 같음
     }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "static",
+          // to: "dist", // from에서 to로 파일 복사할 경로 지정. to는 기본이 output 옵션의 값
+        },
+      ],
+    }),
   ],
+  devServer: {
+    // port: 1234//기본 8080
+  },
 };
