ad67952388011b4e1c3b25e9ac32de73cba260fc
yejinleee <yejin9487@gmail.com>
2023-11-27 14:37:02 +0900

    1124 Vue

---
+++ Vue/49_Vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="!isEdit">
+    {{ msg }}
+    <button @click="onEdit">Edit !</button>
+  </div>
+  <div v-if="isEdit">
+    <input ref="editor" v-model="msg" @keyup.enter="onEdit" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isEdit: false,
+      msg: "hello",
+    };
+  },
+  methods: {
+    onEdit() {
+      this.isEdit = !this.isEdit;
+
+      this.$nextTick(() => {
+        //// setTimeout(() => { 와 같은 역할이다
+        this.$refs.editor.focus();
+      }); // 기본값은 0이라 생략가능. 0초후 발생이기 보단 콜스택 이후
+    },
+  },
+};
+</script>
