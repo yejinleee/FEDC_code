6e8ff5bb959c0e64deb3f409f1879fa2174d08e4
yejinleee <yejin9487@gmail.com>
2023-11-27 17:36:42 +0900

    1127 fetch 받아와서 데이터 변경

--- Vue/50_Vue/Vuex/src/App.vue
+++ Vue/50_Vue/Vuex/src/App.vue
@@ -2,14 +2,18 @@
   <h1>{{ msg }}</h1>
   <Hello />
   <button @click="increaseCount">INCREASE!</button>
+  <World />
+  <button @click="$store.dispatch('fetchTodo')">Update Msg!</button>
 </template>
 
 <script>
 import Hello from "~/components/Hello";
+import World from "~/components/World";
 
 export default {
   components: {
     Hello,
+    World,
   },
   data() {
     return {
@@ -19,6 +23,10 @@ export default {
   methods: {
     increaseCount() {
       this.$store.commit("increaseCount"); /////!! 이전엔 mutations.increaseCount로 썼었음
+    },
+    fetchTodo() {
+      //action을 실행할 땐 dispatch!
+      this.$store.dispatch("fetchTodo");
     },
   },
 };
--- Vue/50_Vue/Vuex/src/store/index.js
+++ Vue/50_Vue/Vuex/src/store/index.js
@@ -8,12 +8,33 @@ export default createStore({
       count: 1,
     };
   },
+  getters: {
+    // computed와 유사
+    reversedMsg(state) {
+      return state.msg.split("").reverse().join("");
+    },
+  },
   mutations: {
     increaseCount(state) {
       // 매개변수로 state가 데이터인 state이다. 이렇게 둬야 내부에서 접근 가능!
       state.count += 1;
     },
+    updateMsg(state, newMsg) {
+      state.msg = newMsg;
+    },
   },
-  actions: {},
-  getters: {}, // computed와 유사!
+  actions: {
+    async fetchTodo(context) {
+      // 다른 actions에 접근할 수 있는 context를 제공
+      // context => state, getters 접근 가능. commit, dispatch로 접근 가능
+      const todo = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      ).then((res) => res.json());
+      console.log(todo.title);
+      // vuex 이전엔 mutations.updateMsg(todo.title);
+      // mutations 사용할 땐 commit 사용
+      context.commit("updateMsg", todo.title); // 실행할 변이메소드명, 인수
+      // fetchTodo({commit}) 으로 구조분해 했다면 그냥 commit으로
+    },
+  },
 });
