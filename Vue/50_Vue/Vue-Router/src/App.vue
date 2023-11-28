b0a7a8686f9409cd3781c522f65b9589013b3ea0
yejinleee <yejin9487@gmail.com>
2023-11-28 13:40:25 +0900

    1127 Vue Router

---
+++ Vue/50_Vue/Vue-Router/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <RouterView />
+  <div>이건 App.vue</div>
+</template>
+
+<script>
+export default {};
+</script>
