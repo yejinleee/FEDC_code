dd76f7ed1ffc7c21b86bb527331f976a6eccd349
yejinleee <yejin9487@gmail.com>
2023-11-29 00:09:35 +0900

    템플릿 오타 수정

--- Vue/webpack-template/src/routes/index.js
+++ Vue/webpack-template/src/routes/index.js
@@ -1,7 +1,7 @@
-import { creaeteRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "./Home";
 
-export default creaeteRouter({
+export default createRouter({
   history: createWebHistory(),
   scrollBehavior: () => ({ top: 0 }), // 일반함수 형태도 가능인데, 화살표함수로가 더 간결
   routes: [
