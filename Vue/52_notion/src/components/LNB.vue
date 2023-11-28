0a7dc6a1203cb4dfbde43965d32aa94a9cf86718
yejinleee <yejin9487@gmail.com>
2023-11-29 01:24:40 +0900

    1129 컴포넌트화와 재귀로 하위 목록 출력, 아이콘

--- Vue/52_notion/src/components/LNB.vue
+++ Vue/52_notion/src/components/LNB.vue
@@ -5,16 +5,24 @@
       Leon's Notion
     </div>
     <ul>
-      <li v-for="workspace in workspaces" :key="workspace.id">
-        {{ workspace.title }}
-      </li>
+      <WorkspaceItem
+        v-for="workspace in workspaces"
+        :key="workspace.id"
+        :workspace="workspace"
+      />
     </ul>
     <div class="actions"></div>
   </nav>
 </template>
 
 <script>
+import WorkspaceItem from "~/components/WorkspaceItem";
+
 export default {
+  components: {
+    WorkspaceItem,
+    WorkspaceItem,
+  },
   computed: {
     workspaces() {
       return this.$store.state.workspace.workspaces;
---
+++ Vue/52_notion/src/components/WorkspaceItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <li>
+    <div :style="{ paddingLeft: `${14 * depth}px` }" class="title">
+      <span class="material-icons"> play_arrow </span>
+      <span class="text">{{ workspace.title }}</span>
+      <div class="actions">
+        <span class="material-icons"> add </span>
+        <span class="material-icons"> delete </span>
+      </div>
+    </div>
+    <ul v-if="hasChildren">
+      <WorkspaceItem
+        v-for="ws in workspace.documents"
+        :key="ws.id"
+        :workspace="ws"
+        :depth="depth + 1"
+      />
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    workspace: {
+      type: Object,
+      default: () => ({}),
+    },
+    depth: {
+      type: Number,
+      default: 1,
+    },
+  },
+  computed: {
+    hasChildren() {
+      return this.workspace.documents && this.workspace.documents.length;
+    },
+  },
+};
+</script>
