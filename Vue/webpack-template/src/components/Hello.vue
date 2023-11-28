bc5de47fbea091bc2ebe0ac0436addc9d22beffc
yejinleee <yejin9487@gmail.com>
2023-11-28 18:44:38 +0900

    1128 webpack template 상위경로에도 생성

---
+++ Vue/webpack-template/src/components/Hello.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Hello</h1>
+</template>
+<style scoped lang="scss">
+h1 {
+  color: yellow;
+}
+</style>
---
+++ Vue/webpack-template/src/routes/index.js
@@ -0,0 +1,13 @@
+import { creaeteRouter, createWebHistory } from "vue-router";
+import Home from "./Home";
+
+export default creaeteRouter({
+  history: createWebHistory(),
+  scrollBehavior: () => ({ top: 0 }), // 일반함수 형태도 가능인데, 화살표함수로가 더 간결
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+  ],
+});
