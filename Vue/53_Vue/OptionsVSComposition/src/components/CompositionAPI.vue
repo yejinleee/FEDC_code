11fc23c84a12f1708894f8ec542cc89a5d233625
yejinleee <yejin9487@gmail.com>
2023-11-30 15:01:22 +0900

    1130 Composition 비교 - ref

--- Vue/53_Vue/OptionsVSComposition/src/components/CompositionAPI.vue
+++ Vue/53_Vue/OptionsVSComposition/src/components/CompositionAPI.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
-import { watch, reactive } from 'vue';
+import { ref, onMounted } from 'vue';
 
-// const userName = ref('');
-const user = reactive({
-  name: 'happy',
-  age: 20,
+const inputEl = ref<HTMLInputElement | null>(null); // HTML 검색시 못찾으면 null임
+
+onMounted(() => {
+  if (inputEl.value) {
+    // 'inputEl.value' is possibly 'null'.ts(18047)방어
+    // 하지만, null로 초기화해놓고 null이 아닌 조건이니까 'never' 가 됨
+    // Property 'focus' does not exist on type 'never'.ts(2339)
+    // 그래서  const inputEl = ref<HTMLInputElement | null>(null); 로 해줘야함
+    inputEl.value.focus();
+  }
 });
-
-watch(
-  // user.name 변경만 감지하고 싶을 때
-  () => user.name,
-  (newVal) => console.log(newVal),
-);
-
-console.log(user.name, user.age);
 </script>
 
 <template>
-  <input v-model="user.name" />
-  <h1>{{ user.name }}</h1>
+  <input ref="inputEl" />
 </template>
--- Vue/53_Vue/OptionsVSComposition/src/components/OptionsAPI.vue
+++ Vue/53_Vue/OptionsVSComposition/src/components/OptionsAPI.vue
@@ -2,46 +2,15 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  inheritAttrs: false,
-  props: {
-    modelValue: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      default: '', // 선택속성이 됨
-    },
-    active: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  emits: ['update:modelValue'],
-  created() {
-    console.log(this.$props.name, this.$props.active);
-    // props의 경우 this에도 바인딩이 되기 떄문에 중간에 $props 생략해도됨!!ㅇㅁㅇ
-  },
-  methods: {
-    inputHandler(event: Event) {
-      this.$emit('update:modelValue', (event.target as HTMLInputElement).value);
-    },
+  mounted() {
+    if (this.$refs.inputEl) {
+      // Object is of type 'unknown'.ts(2571)방어
+      (this.$refs.inputEl as HTMLInputElement).focus();
+    }
   },
 });
 </script>
 
 <template>
-  <label :class="{ active }">
-    {{ name }}
-    <input
-      v-bind="$attrs"
-      :value="modelValue"
-      @input="inputHandler" />
-  </label>
+  <input ref="inputEl" />
 </template>
-
-<style scoped>
-.active {
-  color: red;
-}
-</style>
