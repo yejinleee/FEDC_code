f756891f8b924b6cbed76ea679f00f41a8f500b9
yejinleee <yejin9487@gmail.com>
2023-11-30 11:37:01 +0900

    1130 Composition 비교 - ref,method,lifecycle

---
+++ Vue/53_Vue/OptionsVSComposition/src/components/CompositionAPI.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'; // mounted는 vue에서 가져와야함
+
+const count = ref(0);
+
+console.log('created'); // script 영역에 적어주는 것만으로도 라이프사이클의 created()순서와 거의 비슷!
+onMounted(() => {
+  console.log('Mounted');
+});
+
+function increase() {
+  count.value += 1;
+}
+</script>
+
+<template>
+  <h1 @click="increase">{{ count }}</h1>
+</template>
---
+++ Vue/53_Vue/OptionsVSComposition/src/components/OptionsAPI.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue'; // TS 설정 위함
+
+export default defineComponent({
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  created() {
+    console.log('Created!');
+  },
+  mounted() {
+    console.log('mounted!');
+  },
+  methods: {
+    increase() {
+      this.count += 1;
+    },
+  },
+});
+</script>
+
+<template>
+  <h1 @click="increase">{{ count }}</h1>
+</template>
