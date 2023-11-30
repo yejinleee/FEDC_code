b0de5885a866170b9559459af671f73270acc9b9
yejinleee <yejin9487@gmail.com>
2023-11-30 17:18:09 +0900

    1130 TodoList 추가 세팅, 아이콘 컴포넌트

--- Vue/53_Vue/TodoList/src/main.ts
+++ Vue/53_Vue/TodoList/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia';
 import router from './routes';
 
 import App from './App.vue';
+import './main.scss';
 
 const pinia = createPinia();
 createApp(App).use(pinia).use(router).mount('#app');
--- Vue/53_Vue/TodoList/src/routes/MainPage.vue
+++ Vue/53_Vue/TodoList/src/routes/MainPage.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
-(async () => {
-  // 즉시실행함수
-  const res = await fetch('/api/hello', {
-    method: 'POST', //서버리스 함수에 접근할 땐 꼭 POST
-  });
-  const json = await res.json();
-  console.log(json);
-})();
+import TheIcon from '~/components/TheIcon.vue';
 </script>
 
 <template>
   <h1>d여긴 Main Page!</h1>
+  <TheIcon>add</TheIcon>
+  <TheIcon>check</TheIcon>
+  <TheIcon>done_all</TheIcon>
 </template>
