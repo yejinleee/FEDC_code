c115bf11694bc0127e96aaf5afa737813aef9a10
yejinleee <yejin9487@gmail.com>
2023-12-08 01:15:21 +0900

    1208 Progress

--- React/59_react_component/src/components/Progress.js
+++ React/59_react_component/src/components/Progress.js
@@ -23,6 +23,29 @@ const Track = styled.div`
   height: 4px;
   border-radius: 2px;
   background-color: #44b;
+
+  background-size: 20px 20px;
+  background-image: linear-gradient(
+    45deg,
+    rgba(255, 255, 255, 0.15) 25%,
+    transparent 25%,
+    transparent 50%,
+    rgba(255, 255, 255, 0.15) 50%,
+    rgba(255, 255, 255, 0.15) 75%,
+    transparent 75%,
+    transparent 100%
+  );
+
+  animation: move 1000ms linear infinite;
+  transition: width 100ms linear;
+  @keyframes move {
+    from {
+      background-position: 0 0;
+    }
+    to {
+      background-position: 40px 0;
+    }
+  }
 `;
 const Progress = ({ value, onChange, ...props }) => {
   return (
