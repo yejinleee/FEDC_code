acfed478dca821290bc8e981fc894964a1cac5d5
yejinleee <yejin9487@gmail.com>
2023-12-11 16:16:03 +0900

    1211 useClickAway 사소한 성능개선

--- React/60_react_customhook/src/hooks/useClickAway.js
+++ React/60_react_customhook/src/hooks/useClickAway.js
@@ -3,12 +3,20 @@ const events = ["mousedown", "touchstart"];
 
 const useClickAway = (handler) => {
   const ref = useRef(null);
+  const savedHandler = useRef(handler);
+
+  useEffect(() => {
+    // 다시 렌더링되지 않고 ref 값 만 바꾸도록 할 수 있음!
+    savedHandler.current = handler;
+  }, [handler]);
+
   useEffect(() => {
     const element = ref.current;
     if (!element) return;
 
     const handleEvent = (e) => {
-      !element.contains(e.target) && handler(e);
+      !element.contains(e.target) && savedHandler.current(e); // 원래 여기 handler(e)
+      // handler가 바뀔때마다 이벤트를 add하고 remove 했어야했음..
     };
 
     for (const event of events) {
@@ -19,7 +27,7 @@ const useClickAway = (handler) => {
         document.removeEventListener(event, handleEvent);
       }
     };
-  }, [ref, handler]);
+  }, [ref]);
 
   return ref;
 };
